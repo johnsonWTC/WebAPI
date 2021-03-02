f067310511eeaae43cc6e325f9f05e8f87d80cff
Johnson Dubula <jdubula@iqbusiness.net>
2021-03-02 18:00:00 +0300

    modified like

--- WEBAPI/Controllers/WeatherForecastController.cs
+++ WEBAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WEBAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Fredd", "Brain", "Cain", "Cghhalig", "Mid", "Wm", "alaagy", "ot", "Skkdg", "gkkfggkj"
+            "Fred", "Brain", "Cain", "Cghhalig", "Mid", "Wm", "alaagy", "ot", "Skkdg", "gkkfggkj"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
